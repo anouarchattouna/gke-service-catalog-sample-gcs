2eec78e2442fd30edab5e6dc1066680c2b682fec
Anouar Chattouna <anouar.chattouna@gmail.com>
2019-03-22 15:41:28 +0100

    Adding quotes sample app for GCS with Istio

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
 # gke-service-catalog-sample-gcs
 This is a sample application that demonstrates how to use k8s Service Catalog, GCP Service Broker &amp; GCS from a GKE cluster with Istio enabled.
+
+It extends the [quotes](https://github.com/GoogleCloudPlatform/kubernetes-engine-samples/tree/master/service-catalog/cloud-storage) sample application to make it work from an Istio cluster.
+
+## Prerequisites
+
+Successful use of the sample requires:
+
+*   A Kubernetes cluster, minimum version 1.8.x.
+*   Kubernetes Service Catalog and the Google Cloud Platform Service Broker [installed](
+    https://cloud.google.com/kubernetes-engine/docs/how-to/add-on/service-broker/install-service-catalog).
+*   The [Service Catalog
+    CLI](https://github.com/kubernetes-incubator/service-catalog/blob/master/docs/install.md#installing-the-service-catalog-cli)
+    (`svcat`) installed.
+*   A running [installation](https://istio.io/docs/setup/kubernetes/) of Istio
+
+## Step 1: Create a Kubernetes namespace
+```shell
+kubectl apply -f manifests/storage-quotes-namespace.yaml
+```
+
+## Step 2: Provisioning Cloud Storage
+
+This sample application is using a single service account for authentication with all Google Cloud services.
+
+* Create the cloud-iam-service-account istance
+```shell
+kubectl apply -f manifests/gcp-iam-sa/instance.yaml
+```
+The instance is provisioned when status is `Ready` : ```svcat get instances --namespace storage-quotes```
+* Create the binding to the cloud-iam-service-account istance
+```shell
+kubectl apply -f manifests/gcp-iam-sa/binding.yaml
+```
+Wait for the binding to be `Ready` : ```svcat get bindings --namespace storage-quotes```
+* Once the instance and the binding are `Ready`, make sure to have the following secrets:
+```
+kubectl get secrets -n storage-quotes
+NAME                               TYPE                                  DATA      AGE
+gcp-iam-sa-credentials             Opaque                                2         1h
+quotes-gcp-gcs-credentials         Opaque                                3         1h                                                                                                                                                                                                                                        
+```
+## Step 3: Deploy the application
+```shell
+kubectl apply -f manifests/quotes-deployment.yaml
+```
+## Step 3: Access the application
+Now that the application is up and running, we need to make the application accessible from outside of your Kubernetes cluster. An [Istio Gateway](https://istio.io/docs/concepts/traffic-management/#gateways) is used for this purpose.
+```shell
+kubectl apply -f manifests/quotes-gateway.yaml
+```
+### Determining the ingress IP and ports
+Execute the following command to determine if your Kubernetes cluster is running in an environment that supports external load balancers:
+```shell
+kubectl get svc istio-ingressgateway -n istio-system
+NAME                   TYPE           CLUSTER-IP       EXTERNAL-IP     PORT(S)                                      AGE
+istio-ingressgateway   LoadBalancer   172.21.109.129   130.211.10.121  80:31380/TCP,443:31390/TCP,31400:31400/TCP   17h
+```
+If the EXTERNAL-IP value is set, your environment has an external load balancer that you can use for the ingress gateway.
+
+```$ export GATEWAY_URL=$EXTERNAL-IP```
+```shell
+# Query the quotes:
+curl -s http://${GATEWAY_URL}/quotes
+{"quotes":[]}
+```
+```shell
+# Create a new quote:
+curl http://${GATEWAY_URL}/quotes -d '{"person": "Dalai Lama", "quote": "Be kind whenever possible. It is always possible."}'
+
+# Query the quotes again:
+curl http://${GATEWAY_URL}/quotes
+{"quotes":[{"person":"Dalai Lama","quote":"Be kind whenever possible. It is always possible."}]}
+```
+
+Et voilà !
+
+## Credits
+
+- [GoogleCloudPlatform/kubernetes-engine-samples](https://github.com/GoogleCloudPlatform/kubernetes-engine-samples/tree/master/service-catalog#application-service-account)
+- [istio.io](https://istio.io/docs/reference/config/networking/v1alpha3)
+- [istio.io/bookinfo](https://istio.io/docs/examples/bookinfo/)
---
+++ quotes/main.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+	"cloud.google.com/go/storage"
+	"github.com/gorilla/mux"
+	"golang.org/x/net/context"
+	"google.golang.org/api/iterator"
+)
+
+const (
+	storageBucket = "GOOGLE_CLOUD_STORAGE_BUCKET_ID"
+	port          = "8080"
+)
+
+var bucketName string
+
+type quote struct {
+	Person string `json:"person,omitempty"`
+	Quote  string `json:"quote,omitempty"`
+}
+
+type quotes struct {
+	Quotes []quote `json:"quotes"`
+}
+
+func main() {
+	bucketName = os.Getenv(storageBucket)
+	if bucketName == "" {
+		log.Fatalf("%s environment variable unspecified. Please provide Storage bucket.", storageBucket)
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/quotes", handleCreateQuote).Methods("POST")
+	r.HandleFunc("/quotes", handleListQuotes).Methods("GET")
+
+	log.Println("Server listening on port " + port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
+
+func handleCreateQuote(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Printf("Failed to create client: %v", err)
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("Error reading request body: %v", err)
+		writeError(w, http.StatusBadRequest, "cannot read the request body")
+		return
+	}
+	var e quote
+	if err := json.Unmarshal(body, &e); err != nil {
+		log.Printf("Error parsing request body: %v", err)
+		writeError(w, http.StatusBadRequest, "cannot parse the request body - invalid JSON")
+		return
+	}
+	if e.Person == "" {
+		writeError(w, http.StatusBadRequest, "missing required property: \"Person\"")
+		return
+	}
+	if e.Quote == "" {
+		writeError(w, http.StatusBadRequest, "missing required property: \"Quote\"")
+		return
+	}
+
+	wc := client.Bucket(bucketName).Object(e.Person).NewWriter(ctx)
+	if _, err := wc.Write([]byte(e.Quote)); err != nil {
+		log.Printf("Unable to write quote %q to bucket %q, object %q: %v", e.Quote, bucketName, e.Person, err)
+		writeError(w, http.StatusInternalServerError, "internal server error")
+		return
+	}
+
+	if err := wc.Close(); err != nil {
+		log.Printf("Unable to close writer: %v", err)
+		writeError(w, http.StatusInternalServerError, "internal server error")
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(body)
+}
+
+func handleListQuotes(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Printf("Failed to create client: %v", err)
+		return
+	}
+
+	it := client.Bucket(bucketName).Objects(ctx, nil)
+	e := quotes{Quotes: []quote{}}
+	for {
+		attrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Printf("Error reading next object: %v", err)
+			writeError(w, http.StatusInternalServerError, "internal server error")
+			return
+		}
+
+		objectName := attrs.Name
+		q, err := readObject(ctx, client, objectName)
+		if err != nil {
+			writeError(w, http.StatusInternalServerError, "internal server error")
+			return
+		}
+		e.Quotes = append(e.Quotes, quote{Person: objectName, Quote: q})
+	}
+
+	j, err := json.Marshal(e)
+	if err != nil {
+		log.Printf("Error marshaling JSON for input %+v: %v", e, err)
+		writeError(w, http.StatusInternalServerError, "internal server error")
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(j)
+}
+
+func writeError(w http.ResponseWriter, status int, message string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	j, err := json.Marshal(
+		struct {
+			Error string `json:"error"`
+		}{
+			Error: message,
+		})
+	if err != nil {
+		w.Write([]byte("{\"error\": \"internal server error\"}"))
+		return
+	}
+	w.Write(j)
+}
+
+func readObject(ctx context.Context, client *storage.Client, objectName string) (string, error) {
+	rc, err := client.Bucket(bucketName).Object(objectName).NewReader(ctx)
+	if err != nil {
+		log.Printf("Error creating a reader for bucket %q, object %q: %v", bucketName, objectName, err)
+		return "", err
+	}
+	defer rc.Close()
+
+	data, err := ioutil.ReadAll(rc)
+	if err != nil {
+		log.Printf("Error reading contents of bucket %q, object %q: %v", objectName, bucketName, err)
+		return "", err
+	}
+	return string(data), nil
+}
